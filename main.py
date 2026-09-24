92c11f7561824cf0ffa2795511fad777827b64af
danixdy8 <danixdy8@gmail.com>
2026-09-24 00:02:46 -0500

    Refactor FastAPI app and update routes

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 from fastapi import FastAPI
 import models 
-from db import create_db_and_tables
-from routers import usuarios, libros
+from db import create_all_tables
+from routes import users
 
 app = FastAPI(
-    title="API de Gestión con EC2 y RDS", 
-    lifespan=create_db_and_tables
+    title="API EC2 y RDS",
+    lifespan=create_all_tables
 )
 
-app.include_router(usuarios.router)
-app.include_router(libros.router)
+app.include_router(users)
 
 @app.get("/")
 def raiz():
-    return {"estado": "API operativa", "documentacion": "/docs"}
+    return {"estado": "API operativa", "docs": "/docs"}
